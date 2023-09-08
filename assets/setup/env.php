ab7b097277b061fb1cddadd2511a2633d857bace
izza iqbal <65022161+izzybug@users.noreply.github.com>
2023-09-08 20:51:38 +0700

    1

---
+++ assets/setup/env.php
@@ -0,0 +1,21 @@
+<?php
+
+if (!defined('APP_NAME'))                       define('APP_NAME', 'SIPARTI');
+if (!defined('APP_NAME_LONG'))                  define('APP_NAME_LONG', 'Sistem Informasi Perizinan Karya Tulis Ilmiah');
+if (!defined('APP_ORGANIZATION'))               define('APP_ORGANIZATION', 'KLiK');
+if (!defined('APP_OWNER'))                      define('APP_OWNER', 'msaad1999');
+if (!defined('APP_DESCRIPTION'))                define('APP_DESCRIPTION', 'Embeddable PHP Login System');
+
+if (!defined('ALLOWED_INACTIVITY_TIME'))        define('ALLOWED_INACTIVITY_TIME', time()+1*60);
+
+if (!defined('DB_DATABASE'))                    define('DB_DATABASE', 'klik_loginsystem');
+if (!defined('DB_HOST'))                        define('DB_HOST','localhost');
+if (!defined('DB_USERNAME'))                    define('DB_USERNAME','root');
+if (!defined('DB_PASSWORD'))                    define('DB_PASSWORD' ,'');
+if (!defined('DB_PORT'))                        define('DB_PORT' ,'');
+
+if (!defined('MAIL_HOST'))                      define('MAIL_HOST', 'smtp.gmail.com');
+if (!defined('MAIL_USERNAME'))                  define('MAIL_USERNAME', 'siparti.b2@gmail.com');
+if (!defined('MAIL_PASSWORD'))                  define('MAIL_PASSWORD', 'oljwctlbgkrmowpj');
+if (!defined('MAIL_ENCRYPTION'))                define('MAIL_ENCRYPTION', 'ssl');
+if (!defined('MAIL_PORT'))                      define('MAIL_PORT', 465);
